91073fac4325d14b2246882d2285c021feaf04fe
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-29 16:11:10 +0200

    [UPD] Imported apptitle component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
+<!-- SCRIPT = JS -->
 <script>
+  import TitleComponent from "./components/AppTitle.vue";
   export default {
     name: 'myApp',
 };
 </script>
+<!-- TEMPLATE = HTML -->
 <template></template>
+<!-- STYLE = CSS -->
 <style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,8 @@
+<!-- Nuovo componente per il titolo -->
+<script>
+  export default {
+    name: 'Title',
+};
+</script>
+<template></template>
+<style></style>
