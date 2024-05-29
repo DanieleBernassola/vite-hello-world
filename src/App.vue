21569ad736836e16696744f519ce34053b7c97f3
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-29 16:30:18 +0200

    [UPD] Added titleComponent in template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <!-- SCRIPT = JS -->
 <script>
-  import TitleComponent from "./components/AppTitle.vue";
-  export default {
-    name: 'myApp',
+import TitleComponent from "./components/AppTitle.vue";
+export default {
+  name: 'myApp',
+  data() {
+    return {
+
+    };
+  },
+  // COMPONENTI CHE IMPORTO
+  components: {
+    TitleComponent,
+  },
 };
 </script>
 <!-- TEMPLATE = HTML -->
-<template></template>
+<template>
+  <!-- POSSO UTILIZZARE IL COMPONENTE IMPORTATO COME TAG HTML -->
+  <TitleComponent></TitleComponent>
+</template>
 <!-- STYLE = CSS -->
 <style></style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,8 +1,10 @@
 <!-- Nuovo componente per il titolo -->
 <script>
-  export default {
-    name: 'Title',
+export default {
+  name: 'Title',
 };
 </script>
-<template></template>
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
 <style></style>
